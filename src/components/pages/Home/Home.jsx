a7359e9c1671d19ba776bd57750afba9ac7ebbb6
Silvar925 <silvarmoonlight@gmail.com>
2024-04-12 10:10:33 +0300

    V1.0.8

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -5,6 +5,7 @@ import { Footer } from "../../widgets/Footer/Footer"
 import { gallery } from "../../data"
 
 export const Home = () => {
+
     return (
         <>
             <Header />
@@ -13,125 +14,40 @@ export const Home = () => {
                 <Filter />
 
                 <div className={styles.gallery}>
-
-                    <div className={styles.rowBox}>
-                        <div className={styles.row}>
-                            <img src="home/img1.png" alt="#" />
-                            <img src="home/img2.png" alt="#" />
-                        </div>
-
-                        <div className={styles.row2}>
-                            <div className={styles.row2Col}>
-                                <img src="home/img3.png" alt="#" />
-                                <img src="home/img4.png" alt="#" />
-                            </div>
-
-                            <img src="home/img5.png" alt="#" />
-                        </div>
-                    </div>
-
-                    <div className={styles.rowBox}>
-                        <div className={styles.row}>
-                            <img src="home/img6.png" alt="#" />
-                            <img src="home/img7.png" alt="#" />
-                        </div>
-
-                        <div className={styles.row2}>
-                            <div className={styles.row2Col}>
-                                <img src="home/img8.png" alt="#" />
-                                <img src="home/img9.png" alt="#" />
-                            </div>
-
-                            <img src="home/img10.png" alt="#" />
-                        </div>
-                    </div>
-
-                    <div className={styles.rowBox}>
-                        <div className={styles.row}>
-                            <img src="home/img11.png" alt="#" />
-                            <img src="home/img12.png" alt="#" />
-                        </div>
-
-                        <div className={styles.row2}>
-                            <div className={styles.row2Col}>
-                                <img src="home/img13.png" alt="#" />
-                                <img src="home/img14.png" alt="#" />
-                            </div>
-
-                            <img src="home/img15.png" alt="#" />
-                        </div>
-                    </div>
-
-                    <div className={styles.rowBox}>
-                        <div className={styles.row}>
-                            <img src="home/img16.png" alt="#" />
-                            <img src="home/img17.png" alt="#" />
-                        </div>
-
-                        <div className={styles.row2}>
-                            <div className={styles.row2Col}>
-                                <img src="home/img18.png" alt="#" />
-                                <img src="home/img19.png" alt="#" />
-                            </div>
-
-                            <img src="home/img20.png" alt="#" />
-                        </div>
-                    </div>
-
-                    <div className={styles.rowBox}>
-                        <div className={styles.row}>
-                            <img src="home/img21.png" alt="#" />
-                            <img src="home/img22.png" alt="#" />
-                        </div>
-
-                        <div className={styles.row2}>
-                            <div className={styles.row2Col}>
-                                <img src="home/img23.png" alt="#" />
-                                <img src="home/img24.png" alt="#" />
-                            </div>
-
-                            <img src="home/img25.png" alt="#" />
-                        </div>
-                    </div>
-
-                    <div className={styles.rowBox}>
-                        <div className={styles.row}>
-                            <img src="home/img26.png" alt="#" />
-                            <img src="home/img27.png" alt="#" />
-                        </div>
-
-                        <div className={styles.row2}>
-                            <div className={styles.row2Col}>
-                                <img src="home/img28.png" alt="#" />
-                                <img src="home/img29.png" alt="#" />
-                            </div>
-
-                            <img src="home/img30.png" alt="#" />
-                        </div>
-                    </div>
-
-                    <div className={styles.rowBox}>
-                        <div className={styles.row}>
-                            <img src="home/img31.png" alt="#" />
-                            <img src="home/img32.png" alt="#" />
-                        </div>
-
-                        <div className={styles.row2}>
-                            <div className={styles.row2Col}>
-                                <img src="home/img33.png" alt="#" />
-                                <img src="home/img34.png" alt="#" />
-                            </div>
-
-                            <img src="home/img35.png" alt="#" />
-                        </div>
-                    </div>
-
+                    {
+                        gallery.map((item, index) => {
+                            return (
+                                row()
+                            )
+                        })
+                    }
                 </div>
+
 
             </section>
 
             <Footer />
-
         </>
+    )
+}
+
+
+const row = () => {
+    return (
+        <div style={{
+            display: 'flex', gap: '20px'
+        }}>
+            temp text
+        </div>
+    )
+}
+
+const rowMirrored = () => {
+    return (
+        <div style={{
+            display: 'flex', gap: '20px'
+        }}>
+
+        </div>
     )
 }
